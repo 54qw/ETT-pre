cf13fb2868fe3f4e726c2116d5750e1eb838d22d
54qw <qianwei@njust.edu.cn>
2025-04-03 22:34:52 +0800

    second commit

---
+++ dataloader.py
@@ -0,0 +1,68 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+from sklearn import preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+
+class TransformerOilTempDataset(Dataset):
+    def __init__(self, data_path, window, is_test=False):
+        # 读取 CSV 数据
+        df = pd.read_csv(data_path)
+        df['date'] = pd.to_datetime(df['date'])  # 转换日期格式
+        df = df.drop(columns=['date'])  # 移除日期列，仅保留数值数据
+
+        # 归一化数据
+        scaler = preprocessing.MinMaxScaler()
+        df_scaled = scaler.fit_transform(df)
+        df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
+
+        features = df_scaled
+        seq_len = window
+        amount_of_features = len(features.columns)  #有几列
+        features = features.values  #pd.DataFrame(stock) 表格转化为矩阵
+        sequence_length = seq_len + 1   #序列长度
+
+        data = []
+        # 生成时间序列样本
+        for index in range(len(features) - sequence_length):
+            data.append(features[index: index + sequence_length])
+        data = np.array(data)  # 转换为 NumPy 数组
+
+        # 训练集与测试集划分
+        train_size = int(0.9 * data.shape[0])  # 90% 用于训练，10% 用于测试
+        train_data, test_data = data[:train_size], data[train_size:]
+        x_train = train_data[:, :-1, :-1]
+        y_train = train_data[:, -1, -1]
+        x_test = test_data[:, :-1, :-1]
+        y_test = test_data[:, -1, -1]
+        if not is_test:
+            self.data = x_train  # 取前 window 天作为输入
+            self.label = y_train  # 取最后一天的 OT 作为目标值
+        else:
+            self.data = x_test
+            self.label = y_test
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return torch.from_numpy(self.data[idx]).to(torch.float32), torch.FloatTensor([self.label[idx]])
+
+if __name__ == '__main__':
+    dataset = TransformerOilTempDataset('./ETDataset/ETT-small/ETTm1.csv', window=10, is_test=False)
+    print(f"Dataset size: {len(dataset)}")  # 确保数据量正常
+    print(f"Sample input shape: {dataset[0][0].shape}")  # 确保输入 shape 正确
+    print(f"Sample output shape: {dataset[0][1].shape}")  # 确保标签 shape 正确
+
+
+    # df = pd.read_csv("./ETDataset/ETT-small/ETTm1.csv", parse_dates=["date"])
+    # # 设置时间索引
+    # df.set_index("date", inplace=True)
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(df.index, df["OT"], label="Oil Temperature (OT)")
+    # plt.xlabel("Time")
+    # plt.ylabel("Temperature")
+    # plt.title("Transformer Oil Temperature Over Time")
+    # plt.legend()
+    # plt.show()
---
+++ train.py
@@ -0,0 +1,80 @@
+from dataloader import TransformerOilTempDataset
+from torch.utils.data import DataLoader
+from model import *
+import torch.nn as nn
+import os
+
+if __name__ == '__main__':
+    device = torch.device('cuda')
+
+    if not os.path.exists("result_picture"):
+        os.makedirs("result_picture")
+
+    if not os.path.exists("best_model"):
+        os.makedirs("best_model")
+
+
+    def parse_args():
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--model", "-m", default='Base', help="which model")
+        args = parser.parse_args()
+        return args
+
+
+    args = parse_args()
+
+    train_data = TransformerOilTempDataset('./ETDataset/ETT-small/ETTm1.csv', 10, is_test=False)
+    test_data = TransformerOilTempDataset('./ETDataset/ETT-small/ETTm1.csv', 10, is_test=True)
+    print_step = 10
+
+    train_loader = DataLoader(train_data, batch_size=256, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_data, batch_size=256, shuffle=True, num_workers=4)
+
+    model_dict = {
+        "Base": CNNLSTMModel,
+        "SE": CNNLSTMModel_SE,
+        "ECA": CNNLSTMModel_ECA,
+        "CBAM": CNNLSTMModel_CBAM,
+        "HW": CNNLSTMModel_HW
+    }
+
+    model = model_dict[args.model]()
+    model = model.to(device)
+
+    print(model)
+    print(f"training model is {args.model}")
+    criterion = nn.MSELoss()
+    criterion = criterion.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    min_loss = 99999
+    for epoch in range(100):
+        print(f'epoch:{epoch}')
+        running_loss = 0.0
+        for step, (data, label) in enumerate(train_loader):
+            data = data.to(device)
+            label = label.to(device)
+            out = model(data)
+            loss = criterion(out, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if step % print_step == 0:  # 每500个batch打印一次训练状态
+                with torch.no_grad():
+                    mse_loss = 0.0
+                    for data, label in test_loader:
+                        data = data.to(device)
+                        label = label.to(device)
+                        out = model(data)
+                        loss = criterion(out, label)
+                        mse_loss += loss.item()
+                    if mse_loss / len(test_loader) < min_loss:
+                        torch.save(model.state_dict(), f"best_model/{args.model}_best.pth")
+                        print("save_best")
+                        min_loss = mse_loss / len(test_loader)
+                    print(f"step:{step}, test loss:{mse_loss / len(test_loader)}")
+
+    print("done")
